a65e1d7b34b19c8d136e4249b48fcb6cff35a00c
N1CK145 <42550633+N1CK145@users.noreply.github.com>
2021-12-30 01:56:47 +0100

    Update delete_hanging.py
    Maybe you dont want to share this...

--- delete_hanging.py
+++ delete_hanging.py
@@ -1,9 +1,5 @@
 from pyEarnapp import EarnApp
-AUTH_TOKENS = [
-    "1%2F%2F0dKMJwG9ekzH1CgYIARAAGA0SNwF-L9Ir3TbDTUaua2-8KR7IWQd7Jr7AwIB1XFNGyWLXhYVXi30xy84m5JklfkOjzT3xMQloKS8",
-    "1%2F%2F0dahtCNwckwNECgYIARAAGA0SNgF-L9IrV6aIVrOfyz-IYo-OD9ew7oQ5Mn2k0IgSRdOcZzNjhnU7oP-b8H2rN7qDKaeG6d7XkA",
-    "1%2F%2F0dtI2hxltQ6iPCgYIARAAGA0SNwF-L9IrrCqtZoZeEH6Tsg9uG8KaDHwz6N1tFTBbUl4EE9gheb-6VhUWxqvQrcCcN2qW34TUJzo"
-]
+AUTH_TOKENS = []
 
 for auth in AUTH_TOKENS:
     api = EarnApp(auth)
